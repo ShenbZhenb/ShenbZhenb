4a84043a815fa65a0a9ddb32738866c52ae8a51f
Shendik <e.shendik@itg-soft.by>
2022-07-07 20:36:00 +0300

    change files

--- 558arpby/libs/main.js
+++ 558arpby/libs/main.js
@@ -89,4 +89,30 @@ burgerToggler();
     }
   })();
 
-  
+  function Rotator(start_from){
+    var phrases = ["Ремонт авиационной техники","Сервисное обслуживание авиационной техники","Производство деталей и авиакомпонентов"];
+    var total = phrases.length;
+    var interval = 4000;
+    if(void 0 === start_from){
+        start_from = 0;
+    }
+ 
+    $(".textrotator").text(phrases[start_from]).animate({"opacity":"1"}, 1000, function(){
+                if(start_from >= (total-1)){
+                    setTimeout(function(){
+                        $(".textrotator").animate({"opacity":"0"}, 1000, function(){
+                            Rotator();
+                        });
+                    }, interval);
+                }else{
+                    start_from++;
+                    setTimeout(function(){
+                        $(".textrotator").animate({"opacity":"0"}, 1000,function(){
+                            Rotator(start_from);
+                        });
+                    }, interval);
+         
+                }           
+         
+    })
+}
