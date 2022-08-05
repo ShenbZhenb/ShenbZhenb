a0e94074de07704ede65c3443b9601e759262b9b
ShenbZhenb <98545945+ShenbZhenb@users.noreply.github.com>
2022-08-05 13:43:41 +0300

    change files

--- libs/main.js
+++ libs/main.js
@@ -75,14 +75,14 @@ burgerToggler();
     let _h = window.innerHeight / 2;
     let _mouseX = e.clientX;
     let _mouseY = e.clientY;
-    let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${
+    let _depth1 = `${400 - (_mouseX - _w) * 0.01}% ${
     50 - (_mouseY - _h) * 0.01
     }%`;
-    let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${
-    50 - (_mouseY - _h) * 0.02
+    let _depth2 = `${400 - (_mouseX - _w) * 0.01}% ${
+    50 - (_mouseY - _h) * 0.01
     }%`;
-    let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${
-    50 - (_mouseY - _h) * 0.06
+    let _depth3 = `${400 - (_mouseX - _w) * 0.01}% ${
+    50 - (_mouseY - _h) * 0.01
     }%`;
     let x = `${_depth3}, ${_depth2}, ${_depth1}`;
     console.log(x);
